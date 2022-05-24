ad184d002db50c337cb15e637d6ed183c90ae106
riccardompesce <riccardompesce@gmail.com>
2022-05-24 18:08:49 +0200

    * Amended schema of device (small fix) * Adds mongo device CRUD operations * To continue

--- app/models/device_schema.py
+++ app/models/device_schema.py
@@ -9,14 +9,14 @@ class DeviceSchema(BaseModel):
     publish_qos: int = Field(..., ge=0, le=2)
     status: bool = Field(...)
     update_datetime: float = Field(..., ge=0.0)
-    decading_factor: Optional[float] = Field()
+    decading_factor: Optional[float]
 
     class Config:
         schema_extra = {
             "example": {
                 "device_id": "D1927821-D37B-4C52-A5D3-0579CCFFC6B6",
                 "measure": "temperature-room24",
-                "publish-qos": 1,
+                "publish_qos": 1,
                 "status": "on",
                 "update_datetime": 1653384081.510052
             }
@@ -34,7 +34,7 @@ class UpdateDeviceModel(BaseModel):
             "example": {
                 "device_id": "D1927821-D37B-4C52-A5D3-0579CCFFC6B6",
                 "measure": "luminosity-room2",
-                "publish-qos": 1,
+                "publish_qos": 1,
                 "status": "off",
                 "update_datetime": 1653384081.540054
             }
