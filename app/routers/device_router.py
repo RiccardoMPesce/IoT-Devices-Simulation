f62ca71377c1b41fba41a11246755f710ebb00ba
riccardompesce <riccardompesce@gmail.com>
2022-05-26 10:52:33 +0200

    Updated common utils and started services

--- app/routers/device_router.py
+++ app/routers/device_router.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 
-from typing import Optional
+from typing import Optional, Union
 
 from services.device_service import *
 
 router = APIRouter()
 
 @router.post("/", response_description="Device data added into the database")
-async def add_device(measure: str, publish_qos: int, decading_factor: Optional[float] = None):
-    return await add_device(measure, publish_qos, decading_factor)
+async def add_device(measure: str, publish_qos: int, status: Union[bool, int, str, None], decading_factor: Optional[float] = None):
+    return await add_device(measure, publish_qos, status, decading_factor)
--- app/services/device_service.py
+++ app/services/device_service.py
@@ -1,3 +1,8 @@
+from typing import Optional, Union
+from datetime import datetime
+from uuid import uuid4
+
+from common import *
 from models.device_schema import DeviceSchema
 
 from repository.device_repository import *
@@ -5,8 +10,11 @@ from repository.device_repository import *
 async def retrieve_devices():
     pass
 
-async def add_device():
-    pass
+async def add_device(measure: str, publish_qos: int, status: Union[bool, int, str, None], decading_factor: Optional[float] = None):
+    added_timestamp = datetime.utcnow().timestamp()
+    device_id = str(uuid4())
+    return await add_device(measure, publish_qos, status, decading_factor)
+
 
 async def retrieve_device():
     pass
