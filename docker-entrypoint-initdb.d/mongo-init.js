00e1e7b5b552fe1499251ea7d84912b42f570e17
riccardompesce <riccardompesce@gmail.com>
2022-06-08 10:08:02 +0200

    Init script amend

--- docker-entrypoint-initdb.d/mongo-init.js
+++ docker-entrypoint-initdb.d/mongo-init.js
@@ -2,6 +2,8 @@ print("Creating collections and user/password")
 
 db = db.getSiblingDB("conf");
 
+db.grantRolesToUser("admin",[{ role: "root", db: "conf" }])
+
 db.createCollection("devices");
 db.createCollection("measures");
 
