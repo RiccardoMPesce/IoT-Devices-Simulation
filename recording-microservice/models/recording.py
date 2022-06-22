2414b73ca706d6ea9cd1906e56e5cc1acd943c1f
riccardompesce <riccardompesce@gmail.com>
2022-06-22 12:20:18 +0200

    Rearranged code for better modularity

--- recording-microservice/models/recording.py
+++ recording-microservice/models/recording.py
@@ -1,9 +1,49 @@
-from pydantic import BaseModel
-from typing import Optional
+import ormar
 
-class MeasureRecording(BaseModel):
-    token: str
-    username: Optional[str]
+from db.database import BaseMeta
 
-    class Config:
-        orm_mode = True
+from datetime import datetime
+
+class FloatRecord(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "float_record"
+
+    recording_id: str = ormar.Integer(primary_key=True)
+    device_id: str = ormar.String(foreign_key=True, unique=True, nullable=False)
+    measure: str = ormar.String(unique=True, nullable=False)
+    is_device_healthy: bool = ormar.Boolean(default=True, nullable=False)
+    timestamp: datetime = ormar.DateTime(nullable=False)
+    value: float = ormar.Float(nullable=False)
+
+class IntRecord(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "int_record"
+
+    recording_id: int = ormar.Integer(primary_key=True)
+    device_id: str = ormar.String(foreign_key=True, unique=True, nullable=False)
+    measure: str = ormar.String(unique=True, nullable=False)
+    is_device_healthy: bool = ormar.Boolean(default=True, nullable=False)
+    timestamp: datetime = ormar.DateTime(nullable=False)
+    value: int = ormar.Integer(nullable=False)
+
+class StringRecord(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "string_record"
+
+    recording_id: int = ormar.Integer(primary_key=True)
+    device_id: str = ormar.String(foreign_key=True, unique=True, nullable=False)
+    measure: str = ormar.String(unique=True, nullable=False)
+    is_device_healthy: bool = ormar.Boolean(default=True, nullable=False)
+    timestamp: datetime = ormar.DateTime(nullable=False)
+    value: str = ormar.String(nullable=False)
+
+class BooleanRecord(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "boolean_record"
+
+    recording_id: int = ormar.Integer(primary_key=True)
+    device_id: str = ormar.String(foreign_key=True, unique=True, nullable=False)
+    measure: str = ormar.String(unique=True, nullable=False)
+    is_device_healthy: bool = ormar.Boolean(default=True, nullable=False)
+    timestamp: datetime = ormar.DateTime(nullable=False)
+    value: bool = ormar.Boolean(nullable=False)
