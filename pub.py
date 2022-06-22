1c2bd2246cdc38ce5f3f857b1692f4839e0008c9
riccardompesce <riccardompesce@gmail.com>
2022-06-22 14:24:41 +0200

    FastAPI mqtt integration to be completed

--- pub.py
+++ pub.py
@@ -1,9 +1,23 @@
-from aiokafka import AIOKafkaProducer
-import asyncio
+import paho.mqtt.client as mqtt
+import time
 
-loop = asyncio.get_event_loop()
+client = mqtt.Client("adafdadff-112d12-112ddd")
 
-producer = AIOKafkaProducer(bootstrap_servers="localhost:9093", loop=loop)
+client.connect("localhost", port=1883, keepalive=120)
+client.subscribe("test/mosquitto/")
 
-for _ in range(100):
-    producer.send("test", b"Test")
+client.loop_start()
+
+client.publish("test/mosquitto/", b"Ciao")
+
+def on_message(client, userdata, message):
+    print("message received " ,str(message.payload.decode("utf-8")))
+    print("message topic=",message.topic)
+    print("message qos=",message.qos)
+    print("message retain flag=",message.retain)
+
+client.on_message = on_message
+
+time.sleep(5)
+
+client.loop_stop()
--- recording-microservice/main.py
+++ recording-microservice/main.py
@@ -2,7 +2,12 @@ import uvicorn
 
 from fastapi import FastAPI
 
+from utils.mqtt import fast_mqtt
+
 app = FastAPI()
+
+# Init MQTT
+fast_mqtt.init_app(app)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8001)
