85095ab07f971ba7e5a18b712121a43574378b3d
riccardompesce <riccardompesce@gmail.com>
2022-06-22 09:15:36 +0200

    Removed pub.py after merging develop

--- pub.py
+++
@@ -1,9 +0,0 @@
-from aiokafka import AIOKafkaProducer
-import asyncio
-
-loop = asyncio.get_event_loop()
-
-producer = AIOKafkaProducer(bootstrap_servers="localhost:9093", loop=loop)
-
-for _ in range(100):
-    producer.send("test", b"Test")
